e4f44c24d2d7af5cabb5a175f88c823a1ed265aa
Devika-Yadav <devikayadavavula@gmail.com>
2025-01-09 23:05:26 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Fragment } from 'react';
+import './App.css';
+import Login from './components/Login'; // Correct if the file is Login.js
+
+
+
+function App() {
+  
+  return (
+    <Fragment>
+      <Login/>
+
+
+    
+    </Fragment>
+  );
+};
+  
+
+
+export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import './Login.css';
+let login = () => {
+    return (
+        <React.Fragment>
+            <div className='login-container'>
+             <header>
+            <h1 className="heading">DYNAMIC ROUTE OPTIMIZATION</h1>
+            <div className="description">
+                This platform, tailored for the Delhi Transport Corporation (DTC), streamlines bus fleet management for Admins, Drivers, and Passengers. It offers real-time tracking, dynamic route optimization, and traffic updates to ensure timely and efficient transit services. Powered by AI and IoT, it enhances operational efficiency and commuter experience.
+            </div>
+            
+        </header>
+        
+        <div className="login">
+            <h1>Login</h1>
+            <form>
+                <div className="form-group">
+                <div className="list-box-container">
+                    <label for="user-role" className="list-label">Select Role</label>
+                    <select id="user-role" className="custom-list-box">
+                        <option value="admin">Admin</option>
+                        <option value="driver">Driver</option>
+                        <option value="passenger">Passenger</option>
+                    </select>
+                </div>
+            </div>
+    
+                <div className="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" id="email" placeholder="Enter your email" required/>
+                </div>
+                <div className="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" id="password" placeholder="Enter your password" required/>
+                </div>
+                <div className="forgot-password">
+                    <a href="/forgot-project">Forgot Password?</a>
+                </div>
+                
+                <button type="submit" class="login-btn">Login</button>
+                <p className='or' >------or------</p>
+                <div class="forgot-password">
+                    <p className='signup-login' > Doesn't have an account ? <a href="/signup">SignUp</a></p>
+                    
+                </div>
+                
+
+
+
+            </form>
+        </div>
+        </div>
+
+        </React.Fragment>
+
+    );
+
+};
+
+export default login;
